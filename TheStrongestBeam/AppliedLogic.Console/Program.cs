28e62261a58df37b748453205378f11630e3d4d3
jarlenandres <jarlenandresrojas@gmail.com>
2025-09-17 20:09:37 -0500

    Class and Console App Update

--- TheStrongestBeam/AppliedLogic.Console/Program.cs
+++ TheStrongestBeam/AppliedLogic.Console/Program.cs
@@ -1,92 +1,14 @@
-﻿var beam = string.Empty;
-Console.Write("Enter the beam: ");
-beam = Console.ReadLine();
+﻿using AppliedLogic.Core;
+using System;
 
-if (IsValid(beam!) == true)
+internal class BeamProgram
 {
-    if (CalculateWeight(beam!))
+    private static void Main()
     {
-        Console.WriteLine("The beam supports the weight.");
-    }
-    else
-    {
-        Console.WriteLine("The beam not support the weight.");
+        Console.Write("Enter the beam: ");
+        string beam = Console.ReadLine();
+
+        string result = ResitorBeam.EvaluateBeam(beam);
+        Console.WriteLine(result);
     }
 }
-else
-{
-    Console.WriteLine("The beam is poorly constructed.");
-}
-
-static bool IsValid(string beam)
-{
-    string b = beam!.Substring(0, 1);
-    if (!(b.Equals("%") || b.Equals("&") || b.Equals("#")))
-    {
-        return false;
-    }
-
-    int size = beam.Length;
-    int count = 0;
-    for (int i = 1; i < size; i++)
-    {
-        string position = beam.Substring(1, i + 1);
-        if (!(position.Equals("=") || position.Equals("*")))
-        {
-            return false;
-        }
-        if (position.Equals("*"))
-        {
-            count++;
-        }
-        else
-        {
-            count = 0;
-        }
-        if (count == 2)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-static bool CalculateWeight(string beam)
-{
-    string b = beam!.Substring(0, 1);
-    int size = beam.Length;
-    int total = 0;
-    int segment = 0;
-    for (int i = 1; i < size; i++)
-    {
-        string position = beam.Substring(i, i + 1);
-        if (position.Equals("="))
-        {
-            segment++;
-        }
-        else
-        {
-            total += segment * 3;
-            segment = 0;
-        }
-    }
-    total += segment;
-
-    int weightLimit = 0;
-    switch (b)
-    {
-        case "%":
-            weightLimit = 10;
-            break;
-
-        case "&":
-            weightLimit = 20;
-            break;
-
-        case "#":
-            weightLimit = 90;
-            break;
-    }
-    return weightLimit >= total;
-}
-
---
+++ TheStrongestBeam/AppliedLogic.Core/EvaluateBeam.cs
@@ -0,0 +1,77 @@
+﻿namespace AppliedLogic.Core
+{
+    public class ResitorBeam
+    {
+        public static string EvaluateBeam(string beam)
+        {
+            int count = 0;
+            string content = beam.Substring(0, 1);
+            if (!(content.Equals("%") || content.Equals("&") || content.Equals("#")))
+            {
+                return "The beam is poorly constructed!";
+            }
+
+            int resistanceBase = 0;
+            char baseType = beam[0];
+            switch (baseType)
+            {
+                case '%':
+                    resistanceBase = 10;
+                    break;
+
+                case '&':
+                    resistanceBase = 30;
+                    break;
+
+                case '#':
+                    resistanceBase = 90;
+                    break;
+            }
+
+            if (beam.Length == 1)
+            {
+                return "The beam supports the weight!";
+            }
+
+            string content1 = beam.Substring(1);
+            int totalWeight = 0;
+            int Sequence = 0;
+            int i = 0;
+            for (i = 1; i < content1.Length; i++)
+            {
+                if (content1[i] == '=')
+                {
+                    Sequence++;
+                }
+                else
+                {
+                    totalWeight += Sequence * 3;
+                    Sequence = 0;
+                }
+
+                if (content1[i] == '*')
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                if (count == 2)
+                {
+                    return "The beam is poorly constructed!";
+                }
+            }
+            totalWeight += Sequence;
+
+            if (totalWeight <= resistanceBase)
+            {
+                return "The beam supports the weight!";
+            }
+            else
+            {
+                return "The beam does NOT support the weight!";
+            }
+        }
+    }
+}
